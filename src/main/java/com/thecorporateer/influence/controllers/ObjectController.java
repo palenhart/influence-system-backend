04fc4dc6f0a0b8d8ac1761fc69b5e5f924bedf56
Zollak <zollak@thecorporateer.com>
2017-08-25 12:51:02 +0200

    changed name for divisions, added corporateers to ObjectController

--- src/main/java/com/thecorporateer/influence/controllers/ObjectController.java
+++ src/main/java/com/thecorporateer/influence/controllers/ObjectController.java
@@ -11,16 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.thecorporateer.influence.repositories.CorporateerRepository;
 import com.thecorporateer.influence.repositories.DivisionRepository;
 
 @RestController
 public class ObjectController {
-	
+
 	@Autowired
 	private DivisionRepository divisionRepository;
-	
+	@Autowired
+	private CorporateerRepository corporateerRespository;
+
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "/getDivisions", method = RequestMethod.OPTIONS)
+	@RequestMapping(value = "/divisions", method = RequestMethod.OPTIONS)
 	public ResponseEntity<?> optionsGetDivisions(HttpServletResponse response) {
 		response.setHeader("Allow", "GET,OPTIONS");
 		return ResponseEntity.ok().body(null);
@@ -28,9 +31,23 @@ public class ObjectController {
 
 	@CrossOrigin(origins = "*")
 	@JsonView(Views.Corporateer.class)
-	@RequestMapping(method = RequestMethod.GET, value = "/getDivisions", produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET, value = "/divisions", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> getDivisions() {
 		return ResponseEntity.ok().body(divisionRepository.findAll());
+	}
+
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/corporateers", method = RequestMethod.OPTIONS)
+	public ResponseEntity<?> optionsGetCorporateers(HttpServletResponse response) {
+		response.setHeader("Allow", "GET,OPTIONS");
+		return ResponseEntity.ok().body(null);
+	}
+
+	@CrossOrigin(origins = "*")
+	@JsonView(Views.Corporateer.class)
+	@RequestMapping(method = RequestMethod.GET, value = "/corporateers", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> getCorporateers() {
+		return ResponseEntity.ok().body(corporateerRespository.findAll());
 	}
 
 }
