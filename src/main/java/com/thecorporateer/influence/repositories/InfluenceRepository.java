a51c8215bf7452a8615cbdd65445649d7ab3b909
Zollak <zollak@thecorporateer.com>
2017-09-03 03:12:06 +0200

    removed unneeded parts from repositories

--- src/main/java/com/thecorporateer/influence/repositories/InfluenceRepository.java
+++ src/main/java/com/thecorporateer/influence/repositories/InfluenceRepository.java
@@ -3,7 +3,6 @@ package com.thecorporateer.influence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.thecorporateer.influence.objects.Corporateer;
 import com.thecorporateer.influence.objects.Department;
@@ -13,13 +12,6 @@ import com.thecorporateer.influence.objects.InfluenceType;
 
 @RepositoryRestResource(exported = false)
 public interface InfluenceRepository extends JpaRepository<Influence, Long> {
-
-//	@RestResource(exported = false)
-//	public <S extends Influence> S save(Influence influence);
-//
-//	@Override
-//	@RestResource(exported = false)
-//	public void delete(Influence influence);
 
 	public Influence findByCorporateerAndDepartmentAndDivisionAndType(@Param("corporateer") Corporateer corporateer,
 			@Param("department") Department department, @Param("division") Division division,
--- src/main/java/com/thecorporateer/influence/repositories/TransactionRepository.java
+++ src/main/java/com/thecorporateer/influence/repositories/TransactionRepository.java
@@ -3,20 +3,12 @@ package com.thecorporateer.influence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.thecorporateer.influence.objects.Corporateer;
 import com.thecorporateer.influence.objects.Transaction;
 
 @RepositoryRestResource(exported = false)
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-	
-//	@RestResource(exported = false)
-//	public <S extends Transaction> S save(Transaction transaction);
-//
-//	@Override
-//	@RestResource(exported = false)
-//	public void delete(Transaction transaction);
 
 	public Transaction findBySender(@Param("sender") Corporateer corporateer);
 
