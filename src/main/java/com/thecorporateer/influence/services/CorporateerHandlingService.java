b444baee84502b01ebe2c9c7ebbe694ec3e0ad3d
Zollak <zollak@thecorporateer.com>
2017-09-09 20:37:20 +0200

    refactored division/department

--- src/main/java/com/thecorporateer/influence/services/CorporateerHandlingService.java
+++ src/main/java/com/thecorporateer/influence/services/CorporateerHandlingService.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thecorporateer.influence.objects.Corporateer;
-import com.thecorporateer.influence.objects.Department;
 import com.thecorporateer.influence.objects.Division;
 import com.thecorporateer.influence.objects.Influence;
 import com.thecorporateer.influence.objects.InfluenceType;
 import com.thecorporateer.influence.repositories.CorporateerRepository;
-import com.thecorporateer.influence.repositories.DepartmentRepository;
 import com.thecorporateer.influence.repositories.DivisionRepository;
 import com.thecorporateer.influence.repositories.InfluenceRepository;
 import com.thecorporateer.influence.repositories.InfluenceTypeRepository;
@@ -34,15 +32,12 @@ public class CorporateerHandlingService {
 	@Autowired
 	private CorporateerRepository corporateerRepository;
 	@Autowired
-	private DepartmentRepository departmentRepository;
-	@Autowired
 	private DivisionRepository divisionRepository;
 	@Autowired
 	private InfluenceRepository influenceRepository;
 
 	// private List<Rank> ranks = new ArrayList<>();
 	private List<InfluenceType> types = new ArrayList<>();
-	private List<Department> departments = new ArrayList<>();
 	private List<Division> divisions = new ArrayList<>();
 	private List<Corporateer> corporateers = new ArrayList<>();
 
@@ -71,18 +66,11 @@ public class CorporateerHandlingService {
 	 */
 	private void initializeInfluenceTable(Corporateer corporateer) {
 		refreshInfluenceTypes();
-		refreshDepartments();
 		refreshDivisions();
 
 		List<Influence> influences = new ArrayList<>();
 		for (InfluenceType type : types) {
-			for (Department department : departments) {
-				influences.add(new Influence(corporateer, department, divisions.get(0), type, 0));
-			}
 			for (Division division : divisions) {
-				if (division.getId().longValue() == 1L) {
-					continue;
-				}
 				influences.add(new Influence(corporateer, division.getDepartment(), division, type, 0));
 			}
 		}
@@ -131,13 +119,6 @@ public class CorporateerHandlingService {
 	 */
 	private void refreshInfluenceTypes() {
 		types = influenceTypeRepository.findAll();
-	}
-
-	/**
-	 * Refreshes list of departments from repository
-	 */
-	private void refreshDepartments() {
-		departments = departmentRepository.findAll();
 	}
 
 	/**
