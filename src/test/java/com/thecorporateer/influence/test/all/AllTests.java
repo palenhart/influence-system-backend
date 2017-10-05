8d89b726b7ca415533f3761c8bceb5a6e1c54e98
Zollak <zollak@thecorporateer.com>
2017-10-06 00:33:48 +0200

    fixed transaction tests

--- src/test/java/com/thecorporateer/influence/test/all/AllTests.java
+++ src/test/java/com/thecorporateer/influence/test/all/AllTests.java
@@ -6,10 +6,10 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.thecorporateer.influence.test.datahandling.AllDataHandlingTests;
 import com.thecorporateer.influence.test.influence.AllInfluenceTests;
+import com.thecorporateer.influence.test.transactions.AllTransactionTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllDataHandlingTests.class, AllInfluenceTests.class })
-//@SuiteClasses({ AllTransactionTests.class, AllDataHandlingTests.class, AllInfluenceTests.class })
+@SuiteClasses({ AllTransactionTests.class, AllDataHandlingTests.class, AllInfluenceTests.class })
 public class AllTests {
 
 }
--- src/test/java/com/thecorporateer/influence/test/transactions/AllTransactionTests.java
+++ src/test/java/com/thecorporateer/influence/test/transactions/AllTransactionTests.java
@@ -1,12 +1,12 @@
-//package com.thecorporateer.influence.test.transactions;
-//
-//import org.junit.runner.RunWith;
-//import org.junit.runners.Suite;
-//import org.junit.runners.Suite.SuiteClasses;
-//
-//@RunWith(Suite.class)
-////@SuiteClasses({ TestCreateTransaction.class, TestListTransactions.class })
-//@SuiteClasses({ TestCreateTransaction.class })
-//public class AllTransactionTests {
-//
-//}
+package com.thecorporateer.influence.test.transactions;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+//@SuiteClasses({ TestCreateTransaction.class, TestListTransactions.class })
+@SuiteClasses({ TestCreateTransaction.class })
+public class AllTransactionTests {
+
+}
