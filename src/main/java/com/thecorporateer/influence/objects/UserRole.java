3b46833f8823d49feb3de43a9cd2155e2bffb036
Zollak <zollak@thecorporateer.com>
2017-10-06 05:46:21 +0200

    cleanup

--- src/main/java/com/thecorporateer/influence/objects/UserRole.java
+++ src/main/java/com/thecorporateer/influence/objects/UserRole.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
