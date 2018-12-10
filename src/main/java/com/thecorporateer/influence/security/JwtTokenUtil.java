24af7f07e43f3d8d88264fa251a584d6b5a55b3c
Zollak <zollak@thecorporateer.com>
2018-12-10 16:59:11 +0100

    remove device references due to Java 11 update

--- src/main/java/com/thecorporateer/influence/security/JwtTokenUtil.java
+++ src/main/java/com/thecorporateer/influence/security/JwtTokenUtil.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mobile.device.Device;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +19,7 @@ public class JwtTokenUtil implements Serializable {
 	private static final long serialVersionUID = 749715496978334840L;
 
 	static final String CLAIM_KEY_USERNAME = "sub";
-	static final String CLAIM_KEY_AUDIENCE = "audience";
 	static final String CLAIM_KEY_CREATED = "created";
-
-	private static final String AUDIENCE_UNKNOWN = "unknown";
-	private static final String AUDIENCE_WEB = "web";
-	private static final String AUDIENCE_MOBILE = "mobile";
-	private static final String AUDIENCE_TABLET = "tablet";
 
 	@Value("G75Ewrf2xi")
 	private String secret;
@@ -67,17 +60,6 @@ public class JwtTokenUtil implements Serializable {
 		return expiration;
 	}
 
-	public String getAudienceFromToken(String token) {
-		String audience;
-		try {
-			final Claims claims = getClaimsFromToken(token);
-			audience = (String) claims.get(CLAIM_KEY_AUDIENCE);
-		} catch (Exception e) {
-			audience = null;
-		}
-		return audience;
-	}
-
 	private Claims getClaimsFromToken(String token) {
 		Claims claims;
 		try {
@@ -101,27 +83,14 @@ public class JwtTokenUtil implements Serializable {
 		return (lastPasswordReset != null && created.before(lastPasswordReset));
 	}
 
-	private String generateAudience(Device device) {
-		String audience = AUDIENCE_UNKNOWN;
-		if (device.isNormal()) {
-			audience = AUDIENCE_WEB;
-		} else if (device.isTablet()) {
-			audience = AUDIENCE_TABLET;
-		} else if (device.isMobile()) {
-			audience = AUDIENCE_MOBILE;
-		}
-		return audience;
-	}
+//	private Boolean ignoreTokenExpiration(String token) {
+//		String audience = getAudienceFromToken(token);
+//		return (AUDIENCE_TABLET.equals(audience) || AUDIENCE_MOBILE.equals(audience));
+//	}
 
-	private Boolean ignoreTokenExpiration(String token) {
-		String audience = getAudienceFromToken(token);
-		return (AUDIENCE_TABLET.equals(audience) || AUDIENCE_MOBILE.equals(audience));
-	}
-
-	public String generateToken(UserDetails userDetails, Device device) {
+	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<>();
 		claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
-		claims.put(CLAIM_KEY_AUDIENCE, generateAudience(device));
 		claims.put(CLAIM_KEY_CREATED, new Date());
 		return generateToken(claims);
 	}
@@ -133,8 +102,7 @@ public class JwtTokenUtil implements Serializable {
 
 	public Boolean canTokenBeRefreshed(String token, Date lastPasswordReset) {
 		final Date created = getCreatedDateFromToken(token);
-		return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset)
-				&& (!isTokenExpired(token) || ignoreTokenExpiration(token));
+		return !isCreatedBeforeLastPasswordReset(created, lastPasswordReset) && (!isTokenExpired(token));
 	}
 
 	public String refreshToken(String token) {
